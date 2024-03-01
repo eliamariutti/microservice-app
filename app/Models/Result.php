e2af7cbdcd8e8f69e19166d7b5afed9164ac83a9
eliamariutti <elia.mariutti@gmail.com>
2024-03-01 15:22:13 +0100

    Add Models

---
+++ app/Models/Result.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Result extends Model
+{
+    use HasFactory, HasUuids;
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'custom_job_id',
+        'result',
+    ];
+}
