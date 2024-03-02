fa62e07e02d5b89becf77834c117d0b274a80be3
eliamariutti <elia.mariutti@gmail.com>
2024-03-02 14:44:08 +0100

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Actions\CreateNewJob;
+use App\Actions\GetResults;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/jobs', CreateNewJob::class);
+Route::get('/jobs/{id}', GetResults::class);
